22d2648b5f383f58a5a10e6768f7181d563b0ef6
DanyelC <danyel.clinario@gmail.com>
2021-01-22 21:00:30 -0300

    starting oasis inline mode

--- oasis.py
+++ oasis.py
@@ -10,87 +10,114 @@ from smallgames import hangman
 
 bot = telebot.TeleBot('1322322473:AAEqIreqjYCOaTzdXBUoxj8f_BB1FiteIVo')
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    keyboard1 = [InlineKeyboardButton("Church", callback_data='Church')]
-    keyboard2 = [InlineKeyboardButton("Square", callback_data='Square')]
-    keyboard3 = [InlineKeyboardButton("Market", callback_data='Market')]
+#@bot.message_handler(commands=['start'])
+#def start(message):
+#    keyboard1 = [InlineKeyboardButton("Church", callback_data='Church')]
+#    keyboard2 = [InlineKeyboardButton("Square", callback_data='Square')]
+#    keyboard3 = [InlineKeyboardButton("Market", callback_data='Market')]
     
 # create reply keyboard markup
-    reply_markup = InlineKeyboardMarkup([keyboard1, keyboard2, keyboard3])    # send message with text and appended inline keyboard
-    bot.send_message(message.chat.id,"Where would you like to go?",reply_markup=reply_markup)
-    query_handler(call\)
+#    reply_markup = InlineKeyboardMarkup([keyboard1, keyboard2, keyboard3])    # send message with text and appended inline keyboard
+#    bot.send_message(message.chat.id,"Where would you like to go?",reply_markup=reply_markup)
+#    query_handler(call)
 
 
 #@bot.callback_query_handler(func=lambda call: True)
-def query_handler(call):
-    bot.answer_callback_query(callback_query_id=call.id, text='Moving to '+call.data)
-    
-    keyboard1 = [InlineKeyboardButton("Pray", callback_data='p')]
-    keyboard2 = [InlineKeyboardButton("Run", callback_data='r')]
-    keyboard3 = [InlineKeyboardButton("Sleep", callback_data='s')]
+#def query_handler(call):
+#    bot.answer_callback_query(callback_query_id=call.id, text='Moving to '+call.data)
+#    
+#    keyboard1 = [InlineKeyboardButton("Pray", callback_data='p')]
+#    keyboard2 = [InlineKeyboardButton("Run", callback_data='r')]
+#    keyboard3 = [InlineKeyboardButton("Sleep", callback_data='s')]
     
 # create reply keyboard markup
-    reply_markup = InlineKeyboardMarkup([keyboard1, keyboard2, keyboard3])    # send message with text and appended inline keyboard
-    bot.send_message(call.message.chat.id,"What are going to do now?",reply_markup=reply_markup)
+#    reply_markup = InlineKeyboardMarkup([keyboard1, keyboard2, keyboard3])    # send message with text and appended inline keyboard
+#    bot.send_message(call.message.chat.id,"What are going to do now?",reply_markup=reply_markup)
     #bot.send_message(call.message.chat.id, "teste")
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-    query_handler2(call)
+#    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+#    query_handler2(call)
 
-def query_handler2(call):
-    bot.answer_callback_query(callback_query_id=call.id, text='Answer accepted!')
-    if call.data == 'p':
-        bot.send_message(call.message.chat.id, "Praying huh?")
-        print(call.message.message_id)
-        print(call.message.chat.id)
-        #bot.delete_message(call.message.chat.id, call.message.message_id)
-        print('passou')
+#def query_handler2(call):
+#    bot.answer_callback_query(callback_query_id=call.id, text='Answer accepted!')
+#    if call.data == 'p':
+#        bot.send_message(call.message.chat.id, "Praying huh?")
+#        print(call.message.message_id)
+#        print(call.message.chat.id)
+#        #bot.delete_message(call.message.chat.id, call.message.message_id)
+#        print('passou')
 
-
-#============================================================================================================
-
-#============================================================================================================
-
-@bot.message_handler(content_types=["text"])
-def any_msg(message):
-    keyboardmain = types.InlineKeyboardMarkup(row_width=2)
-    first_button = types.InlineKeyboardButton(text="1button", callback_data="first")
-    second_button = types.InlineKeyboardButton(text="2button", callback_data="second")
-    keyboardmain.add(first_button, second_button)
-    bot.send_message(message.chat.id, "testing kb", reply_markup=keyboardmain)
+@bot.message_handler(commands=['start'])
+def start_main_menu(message):
+    main_keyboard = types.InlineKeyboardMarkup(row_width=1)
+    first_button = types.InlineKeyboardButton(text="Church", callback_data="church")
+    second_button = types.InlineKeyboardButton(text="Square", callback_data="square")
+    third_button = types.InlineKeyboardButton(text="Market", callback_data="market")
+    main_keyboard.add(first_button, second_button, third_button)
+    bot.send_message(message.chat.id, "Where would you like to go?", reply_markup=main_keyboard)
 
 @bot.callback_query_handler(func=lambda call:True)
 def callback_inline(call):
-    if call.data == "mainmenu":
+    if call.data == "main_menu":
 
-        keyboardmain = types.InlineKeyboardMarkup(row_width=2)
-        first_button = types.InlineKeyboardButton(text="1button", callback_data="first")
-        second_button = types.InlineKeyboardButton(text="2button", callback_data="second")
-        keyboardmain.add(first_button, second_button)
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="menu",reply_markup=keyboardmain)
+        main_keyboard = types.InlineKeyboardMarkup(row_width=1)
+        first_button = types.InlineKeyboardButton(text="Church", callback_data="church")
+        second_button = types.InlineKeyboardButton(text="Square", callback_data="square")
+        third_button = types.InlineKeyboardButton(text="Market", callback_data="market")
+        main_keyboard.add(first_button, second_button, third_button)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="Where would you like to go?",reply_markup=main_keyboard)
 
-    if call.data == "first":
-        keyboard = types.InlineKeyboardMarkup()
-        rele1 = types.InlineKeyboardButton(text="1t", callback_data="1")
-        rele2 = types.InlineKeyboardButton(text="2t", callback_data="2")
-        rele3 = types.InlineKeyboardButton(text="3t", callback_data="3")
-        backbutton = types.InlineKeyboardButton(text="back", callback_data="mainmenu")
-        keyboard.add(rele1, rele2, rele3, backbutton)
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="replaced text",reply_markup=keyboard)
+    if call.data == "church":
+        keyboard = types.InlineKeyboardMarkup(row_width=3)
+        button1 = types.InlineKeyboardButton(text="Pray", callback_data="pray")
+        button2 = types.InlineKeyboardButton(text="Beg for money", callback_data="beg")
+        button3 = types.InlineKeyboardButton(text="Sleep", callback_data="sleep")
+        back_button = types.InlineKeyboardButton(text="Back", callback_data="main_menu")
+        keyboard.add(button1, button2, button3, back_button)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="You are in the Church",reply_markup=keyboard)
 
-    elif call.data == "second":
-        keyboard = types.InlineKeyboardMarkup()
-        rele1 = types.InlineKeyboardButton(text="another layer", callback_data="gg")
-        backbutton = types.InlineKeyboardButton(text="back", callback_data="mainmenu")
-        keyboard.add(rele1,backbutton)
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="replaced text",reply_markup=keyboard)
+    elif call.data == "square":
+        keyboard = types.InlineKeyboardMarkup(row_width=3)
+        button1 = types.InlineKeyboardButton(text="Meet someone", callback_data="meet")
+        button2 = types.InlineKeyboardButton(text="The Tournament", callback_data="tournament")
+        button3 = types.InlineKeyboardButton(text="Relax", callback_data="relax")
+        back_button = types.InlineKeyboardButton(text="Back", callback_data="main_menu")
+        keyboard.add(button1, button2, button3, back_button)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="You are in the Square",reply_markup=keyboard)
 
-    elif call.data == "1" or call.data == "2" or call.data == "3":
-        bot.answer_callback_query(callback_query_id=call.id, show_alert=True, text="alert")
-        keyboard3 = types.InlineKeyboardMarkup()
-        button = types.InlineKeyboardButton(text="lastlayer", callback_data="ll")
-        keyboard3.add(button)
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="last layer",reply_markup=keyboard3)
+
+
+    elif call.data == "market":
+        keyboard = types.InlineKeyboardMarkup(row_width=3)
+        button1 = types.InlineKeyboardButton(text="Buy new Gear", callback_data="buy")
+        button2 = types.InlineKeyboardButton(text="Buy food", callback_data="food")
+        button3 = types.InlineKeyboardButton(text="Stole somene", callback_data="stole")
+        back_button = types.InlineKeyboardButton(text="Back", callback_data="main_menu")
+        keyboard.add(button1, button2, button3, back_button)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="You are in the Market",reply_markup=keyboard)
+
+
+    elif call.data == "tournament":
+        bot.send_message(call.message.chat.id, "_It's a simple tournament. Pay 500 gold and you may win 3000 gold if you kill everyone_", parse_mode="Markdown")
+        keyboard = types.InlineKeyboardMarkup(row_width=3)
+        button1 = types.InlineKeyboardButton(text="Join", callback_data="join")
+        button2 = types.InlineKeyboardButton(text="Deny", callback_data="deny")
+        button3 = types.InlineKeyboardButton(text="Watch", callback_data="watch")
+        back_button = types.InlineKeyboardButton(text="Back", callback_data="main_menu")
+        keyboard.add(button1, button2, button3, back_button)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="*You are reading the tournament rules*",reply_markup=keyboard,parse_mode="Markdown")
+
+
+    elif call.data == "join":
+        bot.send_message(call.message.chat.id, "_You lost 500 gold_", parse_mode="Markdown") 
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="_You joined the tournament. Be sure you read the rules:_",parse_mode="Markdown",reply_markup=None)
+
+
+    #elif call.data == "1" or call.data == "2" or call.data == "3":
+    #    bot.answer_callback_query(callback_query_id=call.id, show_alert=True, text="alert")
+    #    keyboard3 = types.InlineKeyboardMarkup()
+    #    button = types.InlineKeyboardButton(text="lastlayer", callback_data="ll")
+    #    keyboard3.add(button)
+    #    bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="last layer",reply_markup=keyboard3)
 
 
 #============================================================================================================
