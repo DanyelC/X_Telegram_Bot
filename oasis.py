d3c6068b3d8672f756e295db23ed942c9c51dfd4
Danyel Clinário <52963268+DanyelC@users.noreply.github.com>
2021-05-19 17:15:57 -0300

    Update oasis.py

--- oasis.py
+++ oasis.py
@@ -121,9 +121,11 @@ def start_main_menu(message):
         second_button = types.InlineKeyboardButton(text="Square", callback_data="square")
         third_button = types.InlineKeyboardButton(text="Market", callback_data="market")
         main_keyboard.add(first_button, second_button, third_button)
-        #with open(yourpath+'/wheretogo.mp3', 'wb') as audio_file:
-        #    res = tts.synthesize('Where would you like to go?', accept='audio/mp3', voice='en-US_AllisonV3Voice').get_result()
-        #    audio_file.write(res.content)
+        #passar esse arquivo de audio p pasta principal, isso nao muda p ngm
+        with open(yourpath+'/wheretogo.mp3', 'wb') as audio_file:
+            res = tts.synthesize('Where would you like to go?', accept='audio/mp3', voice='en-US_AllisonV3Voice').get_result()
+            audio_file.write(res.content)
+        #...
         wheretogo = open (yourpath+'/wheretogo.mp3', 'rb')
         bot.send_voice(message.chat.id, wheretogo)
         bot.send_message(message.chat.id, "Choose one:", reply_markup=main_keyboard)
