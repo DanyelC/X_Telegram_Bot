7cb33e9247f16b7dde21ccb2ff98f42909762d5b
Danyel Clinário <52963268+DanyelC@users.noreply.github.com>
2021-05-19 18:36:34 -0300

    Update oasis.py

--- oasis.py
+++ oasis.py
@@ -136,7 +136,8 @@ def start_main_menu(message):
         bot.send_voice(message.chat.id, thefutureisoasis)
         
     elif os.path.exists(yp3):
-        pass
+        bot.send_chat_action(message.chat.id, "record_audio")
+        bot.send_chat_action(message.chat.id, "record_audio")
     else:
         with open(yourpath+'/prohibited.mp3', 'wb') as audio_file:
             res = tts.synthesize(str(message.chat.first_name)+'.'+' Who are you? '+ "You don't have any VIP pass. Your entry to Oasis is prohibited.", accept='audio/mp3', voice='en-US_AllisonV3Voice').get_result()
